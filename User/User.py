791b07d5fff79e387104b460bd3a99c1090c6097
Dcbaas <Dcbaas@users.noreply.github.com>
2019-03-16 23:50:07 -0400

    added a message class and a user class

---
+++ User/User.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self,username,ipAddress):
+        self.username = username
+        self.ipAddress = ipAddress
+        return
+    
+    def getUsername():
+        return self.username
+
+    def getIpAddress():
+        return self.ipAddress
+
+    def setUsername(name):
+        self.username = name
+        return 
+
