8ffe0d099db60383bdbb702b71790f932255a2ff
Dcbaas <Dcbaas@users.noreply.github.com>
2019-03-19 11:53:04 -0400

    added better cmd arguments functionality

--- clientMain.py
+++ clientMain.py
@@ -3,9 +3,18 @@
 import Client
 import sys
 
-ipAddress = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
-portNumber = int(sys.argv[2]) if len(sys.argv) > 2 else 8008
-username = sys.argv[3] if len(sys.argv) > 3 else 'Dave'
+ipAddress = 'localhost'
+portNumber = 8008
+username = 'Dave'
+
+if len(sys.argv) == 4:
+    ipAddress = sys.argv[1]
+    portNumber = int(sys.argv[2])
+    username = sys.argv[3]
+elif len(sys.argv) != 0:
+    print('Usage: ./clientMain.py <ip address> <port number> <username>')
+    print('OR: ./clientMain.py')
+    exit(1)
 
 client = Client.Client('localhost',8008, 'Dave')
 client.runClient()
--- serverMain.py
+++ serverMain.py
@@ -5,6 +5,10 @@ import Server
 portNum = 8008
 if len(sys.argv) == 2:
     portNum = int(sys.argv[1])
+elif len(sys.argv) != 0:
+    print('Usage: ./serverMain.py <port number>')
+    print('OR: ./serverMain.py')
+    exit(1)
 
 server = Server.Server(portNum)
 server.runServer()
